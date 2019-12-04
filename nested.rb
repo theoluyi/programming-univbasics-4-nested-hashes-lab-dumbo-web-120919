f3abc979224f05a7241e6d9bbb5f2e8305837e1e
Theo Carney <theoluyi@gmail.com>
2019-12-04 16:53:46 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,13 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     	programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +81,16 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto] = {:known_for => {}, :languages => {}}
+programmer_hash[:yukihiro_matsumoto][:known_for]= "Ruby" 
+programmer_hash[:yukihiro_matsumoto][:languages]= []
+programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
+programmer_hash[:yukihiro_matsumoto][:languages][1] = "C"
+programmer_hash
+
+# lol all this extra work breaking down the assignment into several methods
+# when really it was that I was forgetting to return the hash
 end
 
 def changing_alan
@@ -102,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
      
 end
 
@@ -121,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages].push('Assembly')
+programmer_hash
 end
