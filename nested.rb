e05f567c9520b21f13e53ff8c6ad4ea105372a38
Theo Carney <theoluyi@gmail.com>
2019-12-04 16:26:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -36,6 +35,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     	programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
